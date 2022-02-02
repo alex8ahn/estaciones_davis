da78acf9a2bd6755501933877e4aa172bc1a4396
alex8ahn <alex8a1991@gmail.com>
2022-02-02 13:43:40 -0600

    cambios en el raspberry.py y 02 con las variables que se necesitan

--- raspberry02.py
+++ raspberry02.py
@@ -18,7 +18,7 @@ def bar_in_to_hPa(bar_in):
     
 con = sqlite3.connect('/home/weewx/archive/weewx.sdb')
 
-df = pd.read_sql_query('SELECT rain, inTemp, outTemp, barometer, windDir, windSpeed, datetime from archive order by datetime', con)
+df = pd.read_sql_query('SELECT rain, inTemp, outTemp, barometer, windDir, windSpeed, dewpoint, inHumidity, outHumidity, datetime from archive order by datetime', con)
 
 df["dateTime"] = pd.to_datetime(df['dateTime'], unit="s", utc=True, ).dt.tz_convert('America/Costa_Rica')
 
