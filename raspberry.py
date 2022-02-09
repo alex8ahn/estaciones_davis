da46e216a6364fa769736edd3f7a22175a55484e
alex8ahn <alex8a1991@gmail.com>
2022-02-09 14:12:28 -0600

    cambios en el raspberry.py y 02 con las variables que se necesitan

--- raspberry.py
+++ raspberry.py
@@ -5,7 +5,7 @@ import datetime
 
 def fahr_to_celsius(temp_fahr):
     """Convierte farenhait a celsius
-    
+
     Returna en grados celsius"""
     temp_celsius = (temp_fahr - 32) * 5 / 9
     return temp_celsius
@@ -15,10 +15,10 @@ def bar_in_to_hPa(bar_in):
     """convierte la presion atmosferica de pulgadas de mercurio a milibare Hpa"""
     bar_hPa = bar_in * 33.8638
     return bar_hPa
-    
+
 con = sqlite3.connect('/var/lib/weewx/weewx.sdb')
 
-df = pd.read_sql_query('SELECT rain, inTemp, outTemp, barometer, windDir, windSpeed, dewpoint, inHumidity, outHumidity, datetime from archive order by datetime', con)
+df = pd.read_sql_query('SELECT rain, outTemp, barometer, windDir, windSpeed, dewpoint, inHumidity, datetime from archive order by datetime', con)
 
 df["dateTime"] = pd.to_datetime(df['dateTime'], unit="s", utc=True, ).dt.tz_convert('America/Costa_Rica')
 
@@ -26,7 +26,7 @@ df["inTemp"] = fahr_to_celsius(df["inTemp"])
 
 df["outTemp"] = fahr_to_celsius(df["outTemp"])
 
-df["barometer"] = bar_in_to_hPa(df["barometer"]) 
+df["barometer"] = bar_in_to_hPa(df["barometer"])
 
 df = df.round({'rain': 2, 'inTemp': 2, 'outTemp': 2, 'barometer': 2, 'windDir': 2, 'windSpeed': 2, 'dewpoint': 2, 'inHumidity': 2, 'outHumidity': 2})
 
