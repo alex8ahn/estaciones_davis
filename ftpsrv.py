1226fc64cd7c45334af84b8a8cbd71b6d096b1c9
alex8ahn <alex8a1991@gmail.com>
2022-02-14 10:36:12 -0600

    cambios en el ftpsrv.py

--- ftpsrv.py
+++ ftpsrv.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 
-import ftplib 
+import ftplib
 import os
 
 with ftplib.FTP('201.220.136.101') as ftp:
-    
+
     filename = 'weewx.sdb'
-    
-    try:    
-        ftp.login('davis', 'cenaoscopeco2021')  
-        
+
+    try:
+        ftp.login('davis', 'cenaoscopeco2021')
+        ftp.cwd("C:\raspberry\DAPRUEBA")
         with open(filename, 'rb') as fp:
-            
+
             res = ftp.storlines("STOR " + filename, fp)
-            
+
             if not res.startswith('226 Transfer OK'):
-                
+
                 print('Upload failed')
 
     except ftplib.all_errors as e:
-        print('FTP error:', e) 
+        print('FTP error:', e)
